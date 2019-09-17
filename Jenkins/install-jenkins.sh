295805ed4f8a5dd85a4bdd9e8c690e2d2a7bc163
Clement Liaw <cklliaw@edu.uwaterloo.ca>
2019-09-17 10:21:35 -0400

    Fixed Jenkins TLS bugs

--- Jenkins/install-jenkins.sh
+++ Jenkins/install-jenkins.sh
@@ -1,5 +1,6 @@
 cd "${0%/*}"
 kubectl apply -f manifests/jenkins_namespace.yaml
-#kubectl apply -f manifests/jenkins-mapping.yaml
-# kubectl apply -f manifests/tls
+kubectl apply -f manifests/jenkins-certificate-mapping.yaml
+kubectl apply -f manifests/jenkins-mapping.yaml
+kubectl apply -f manifests/tls
 helm install jenkins stable/jenkins -n jenkins -f manifests/jenkins_vars.yaml
