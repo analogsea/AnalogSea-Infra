f46f97e4ef504ccb5fd8291faed50c5bd0d77596
Clement Liaw <cklliaw@edu.uwaterloo.ca>
2019-09-19 21:32:36 -0400

    Updated Jenkins install/uninstall scripts

--- Jenkins/destroy-jenkins.sh
+++ Jenkins/destroy-jenkins.sh
@@ -1,7 +1,5 @@
 cd "${0%/*}"
 
-kubectl delete -f manifests/tls/jenkins-certificate.yaml
-kubectl delete -f manifests/tls/jenkins-certificate-mapping.yaml
-kubectl delete -f manifests/tls/jenkins-tlscontext.yaml
+kubectl delete -f manifests/tls
 helm uninstall jenkins -n jenkins
 kubectl delete ns jenkins
--- Jenkins/install-jenkins.sh
+++ Jenkins/install-jenkins.sh
@@ -1,6 +1,17 @@
 cd "${0%/*}"
 kubectl apply -f manifests/jenkins_namespace.yaml
-kubectl apply -f manifests/jenkins-certificate-mapping.yaml
-kubectl apply -f manifests/jenkins-mapping.yaml
 kubectl apply -f manifests/tls
 helm install jenkins stable/jenkins -n jenkins -f manifests/jenkins_vars.yaml
+
+kubectl wait --for=condition=available deployment/jenkins -n jenkins > /dev/null 2>&1
+EXIT_CODE=$?
+while [ "$EXIT_CODE" != "0" ]
+do
+  echo "Waiting for Jenkins deployment to become available..."
+  kubectl wait --for=condition=available deployment/jenkins -n jenkins > /dev/null 2>&1
+  EXIT_CODE=$?
+  sleep 4
+done
+echo "Jenkins online, continuing..."
+
+printf $(kubectl get secret --namespace jenkins jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo
