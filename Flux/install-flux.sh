4fb31f806bbb9ebc8955aeea0f5020db480fa799
Clement Liaw <cklliaw@edu.uwaterloo.ca>
2019-10-01 19:54:58 -0400

    Fixed Flux install script GPG_KEY_ID bug

--- Flux/install-flux.sh
+++ Flux/install-flux.sh
@@ -5,7 +5,7 @@ helm repo add fluxcd https://charts.fluxcd.io
 
 gpg --export-secret-keys --armor | kubectl create secret generic flux-gpg-signing-key -n flux --from-file=flux.asc=/dev/stdin
 
-GPG_KEY_ID=$(gpg --list-secret-keys --with-colons 2> /dev/null | grep '^sec:' | cut --delimiter ':' --fields 5)
+GPG_KEY_ID=$(gpg --list-secret-keys --with-colons 2> /dev/null | grep '^sec:' | cut --delimiter ':' --fields 5 | cut -d$ '\n' --fields 2)
 
 export GPG_KEY_ID
 
@@ -13,15 +13,14 @@ cat templates/flux_vars.yaml.template | envsubst > manifests/flux_vars.yaml
 
 if [ -f "manifests/sealed-flux-git-ssh-key.yaml" ]; then
   kubectl apply -f manifests/sealed-flux-git-ssh-key.yaml
-  sleep 8
+  sleep 2
 fi
 
 helm install flux fluxcd/flux -n flux -f manifests/flux_vars.yaml
 
 kubectl wait --for=condition=available deployment/flux -n flux > /dev/null 2>&1
 EXIT_CODE=$?
-while [ "$EXIT_CODE" != "0" ]
-do
+while [ "$EXIT_CODE" != "0" ]; do
   echo "Waiting for Flux deployment to become available..."
   kubectl wait --for=condition=available deployment/flux -n flux > /dev/null 2>&1
   EXIT_CODE=$?
