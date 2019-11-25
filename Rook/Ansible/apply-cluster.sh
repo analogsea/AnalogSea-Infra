f2ecae831f21173316e8e556f7238863c70ff742
Clement Liaw <cklliaw@uwaterloo.ca>
2019-11-25 15:13:14 -0500

    Updated to Rook 1.2

--- Rook/Ansible/apply-cluster.sh
+++ Rook/Ansible/apply-cluster.sh
@@ -5,6 +5,8 @@ kubectl apply -f manifests/common.yaml
 kubectl apply -f manifests/operator.yaml
 kubectl apply -f manifests/cluster-test.yaml
 
-kubectl apply -f manifests/pvc.yaml
+kubectl apply -f manifests/object_store.yaml
+kubectl apply -f manifests/object_store_user.yaml
+kubectl apply -f manifests/block_pools.yaml
 kubectl apply -f manifests/toolbox.yaml
 kubectl apply -f manifests/storageclasses.yaml
--- Rook/old/destroy-cluster.sh
+++ Rook/old/destroy-cluster.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
 cd "${0%/*}"
-# kubectl delete -f manifests/tls/ceph-certificate.yaml
-# kubectl delete -f manifests/tls/ceph-certificate-mapping.yaml
-# kubectl delete -f manifests/tls/ceph-tlscontext.yaml
-# kubectl delete -f manifests/secret.yaml
-# kubectl delete -f manifests/csi_storageclass.yaml
+kubectl delete -f manifests/tls/ceph-certificate.yaml
+kubectl delete -f manifests/tls/ceph-certificate-mapping.yaml
+kubectl delete -f manifests/tls/ceph-tlscontext.yaml
+kubectl delete -f manifests/secret.yaml
+kubectl delete -f manifests/csi_storageclass.yaml
 kubectl -n rook-ceph delete cephcluster rook-ceph
-kubectl delete -f manifests/operator.yaml
+kubectl delete -f manifests/operator_with_csi.yaml
 kubectl delete -f manifests/common.yaml
 
 VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3 source $(which virtualenvwrapper.sh)
