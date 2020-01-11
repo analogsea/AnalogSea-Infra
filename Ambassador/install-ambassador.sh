20d1ad09fbe506525bad181ff3f98595b0f8c311
Clement Liaw <cklliaw@uwaterloo.ca>
2020-01-10 19:35:13 -0500

    Updated Ambassador

--- Ambassador/install-ambassador.sh
+++ Ambassador/install-ambassador.sh
@@ -1,4 +1,4 @@
 cd "${0%/*}"
 kubectl apply -f manifests/ambassador-namespace.yaml
-helm install ambassador-http stable/ambassador -n ambassador -f manifests/ambassador-http-vars.yaml --version v4.4.4
-helm install ambassador-https stable/ambassador -n ambassador -f manifests/ambassador-https-vars.yaml --version v4.4.4
+helm install ambassador-http stable/ambassador -n ambassador -f manifests/ambassador-http-vars.yaml --version v5.3.0
+helm install ambassador-https stable/ambassador -n ambassador -f manifests/ambassador-https-vars.yaml --version v5.3.0
