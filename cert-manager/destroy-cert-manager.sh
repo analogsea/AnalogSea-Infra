95aa96570bfe69b7b6899dcb610e7490c140799c
Clement Liaw <cklliaw@uwaterloo.ca>
2020-01-12 18:20:28 -0500

    Updated to cert-manager 0.12

--- cert-manager/destroy-cert-manager.sh
+++ cert-manager/destroy-cert-manager.sh
@@ -1,6 +1,7 @@
 cd "${0%/*}"
 
-kubectl delete -f manifests/prod/cert-manager/prod-service.yaml
+kubectl delete -f manifests/dev/dev-service.yaml
+kubectl delete -f manifests/prod/prod-service.yaml
+# kubectl delete -f manifests/dev/dev-
 helm uninstall cert-manager -n ambassador
-kubectl delete -f manifests/crds.yaml
-# kubectl delete -f manifests/cert-manager-namespace.yaml
+kubectl delete -f manifests/00-crds.yaml
--- cert-manager/install-cert-manager.sh
+++ cert-manager/install-cert-manager.sh
@@ -2,26 +2,22 @@ cd "${0%/*}"
 # Install the CustomResourceDefinition resources separately
 kubectl apply --validate=false -f manifests/00-crds.yaml
 
-# Create the namespace for cert-manager
-# kubectl apply -f manifests/cert-manager-namespace.yaml
-
-# Label the cert-manager namespace to disable resource validation
-# kubectl label namespace ambassador --overwrite certmanager.k8s.io/disable-validation=true
-
 # Add the Jetstack Helm repository
-helm repo add jetstack https://charts.jetstack.io
+# helm repo add jetstack https://charts.jetstack.io
 
 # Update your local Helm chart repository cache
-helm repo update
+# helm repo update
 
 # Install the cert-manager Helm chart
-helm install cert-manager jetstack/cert-manager -n ambassador --version v0.11.0 -f manifests/cert-manager_vars.yaml
+helm install cert-manager jetstack/cert-manager -n ambassador --version v0.12.0
 
 echo "Waiting for cert-manager to come up..."
-sleep 30
+sleep 25
 
-# Install issuer
-kubectl apply -f manifests/prod/cert-manager/prod-issuer.yaml
+# Install dev issuer and service
+kubectl apply -f manifests/dev/dev-issuer.yaml
+kubectl apply -f manifests/dev/dev-service.yaml
 
-# Install service
-kubectl apply -f manifests/prod/cert-manager/prod-service.yaml
+# Install prod issuer and service
+kubectl apply -f manifests/prod/prod-issuer.yaml
+kubectl apply -f manifests/prod/prod-service.yaml
