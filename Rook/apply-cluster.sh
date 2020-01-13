10408deea838719142db9b783a5b11ab358e9f31
Clement Liaw <cklliaw@uwaterloo.ca>
2020-01-13 16:21:19 -0500

    Various Rook 1.2 fixes

--- Rook/apply-cluster.sh
+++ Rook/apply-cluster.sh
@@ -5,6 +5,30 @@ kubectl apply -f manifests/common.yaml
 kubectl apply -f manifests/operator.yaml
 kubectl apply -f manifests/cluster-test.yaml
 
-kubectl apply -f manifests/pvc.yaml
+kubectl apply -f manifests/block-pools.yaml
 kubectl apply -f manifests/toolbox.yaml
 kubectl apply -f manifests/storageclasses.yaml
+
+kubectl apply -f manifests/tls/ceph-certificate-mapping.yaml
+kubectl apply -f manifests/tls/ceph-certificate.yaml
+kubectl apply -f manifests/tls/ceph-tlscontext.yaml
+kubectl apply -f manifests/tls/ceph-mapping.yaml
+
+kubectl apply -f manifests/tls/rgw-certificate-mapping.yaml
+kubectl apply -f manifests/tls/rgw-certificate.yaml
+kubectl apply -f manifests/tls/rgw-tlscontext.yaml
+kubectl apply -f manifests/tls/rgw-mapping.yaml
+
+# kubectl wait --for=condition=available deployment/rook-ceph-osd-0 -n rook-ceph > /dev/null 2>&1
+# EXIT_CODE=$?
+# while [ "$EXIT_CODE" != "0" ]
+# do
+#   echo "Waiting for Rook deployment to become available..."
+#   kubectl wait --for=condition=available deployment/rook-ceph-osd-0 -n rook-ceph > /dev/null 2>&1
+#   EXIT_CODE=$?
+#   sleep 4
+# done
+# echo "First OSD created, continuing..."
+#
+# kubectl apply -f manifests/object-store.yaml
+# kubectl apply -f manifests/object-store-user.yaml
---
+++ Rook/get-ceph-admin-key.sh
@@ -0,0 +1,2 @@
+cd "${0%/*}"
+kubectl -n rook-ceph get secret rook-ceph-admin-keyring -o jsonpath="{['data']['keyring']}" | base64 --decode && echo
