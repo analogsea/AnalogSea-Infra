0c907cf7bf7503ad76aa7478ff3211b4a4135235
Clement Liaw <cklliaw@uwaterloo.ca>
2020-03-25 15:26:44 -0400

    Updated to Jenkins 1.9.16

--- Jenkins/install-jenkins.sh
+++ Jenkins/install-jenkins.sh
@@ -1,7 +1,7 @@
 cd "${0%/*}"
 kubectl apply -f manifests/jenkins_namespace.yaml
 kubectl apply -f manifests/tls
-helm install jenkins stable/jenkins -n jenkins -f manifests/jenkins_vars.yaml --version v1.9.15
+helm install jenkins stable/jenkins -n jenkins -f manifests/jenkins_vars.yaml --version v1.9.16
 
 kubectl wait --for=condition=available deployment/jenkins -n jenkins > /dev/null 2>&1
 EXIT_CODE=$?
