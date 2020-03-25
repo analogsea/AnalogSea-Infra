6ef91b0b537d9b7a6339cff2101ea3a46ec377b4
Clement Liaw <cklliaw@uwaterloo.ca>
2020-03-25 14:34:35 -0400

    Replaced cert-manager sanity sleep with retry logic

--- cert-manager/install-cert-manager.sh
+++ cert-manager/install-cert-manager.sh
@@ -11,13 +11,43 @@ kubectl apply --validate=false -f manifests/00-crds.yaml
 # Install the cert-manager Helm chart
 helm install cert-manager jetstack/cert-manager -n ambassador --version v0.12.0
 
-echo "Waiting for cert-manager to come up..."
-sleep 25
+
+kubectl wait --for=condition=available deployment/cert-manager-webhook -n ambassador > /dev/null 2>&1
+EXIT_CODE=$?
+while [ "$EXIT_CODE" != "0" ]
+do
+  echo "Waiting for cert-manager-webhook deployment to become available..."
+  kubectl wait --for=condition=available deployment/cert-manager-webhook -n ambassador > /dev/null 2>&1
+  EXIT_CODE=$?
+  sleep 4
+done
+echo "cert-manager-webhook available, continuing..."
 
 # Install dev issuer and service
-kubectl apply -f manifests/dev/dev-issuer.yaml
+kubectl apply -f manifests/dev/dev-issuer.yaml > /dev/null 2>&1
+EXIT_CODE=$?
+while [ "$EXIT_CODE" != "0" ]
+do
+  echo "Trying to apply cert-manager dev issuer..."
+  kubectl apply -f manifests/dev/dev-issuer.yaml > /dev/null 2>&1
+  EXIT_CODE=$?
+  sleep 4
+done
+echo "cert-manager dev issuer available, continuing..."
+
 kubectl apply -f manifests/dev/dev-service.yaml
 
+
 # Install prod issuer and service
-kubectl apply -f manifests/prod/prod-issuer.yaml
+kubectl apply -f manifests/prod/prod-issuer.yaml > /dev/null 2>&1
+EXIT_CODE=$?
+while [ "$EXIT_CODE" != "0" ]
+do
+  echo "Trying to apply cert-manager prod issuer..."
+  kubectl apply -f manifests/prod/prod-issuer.yaml > /dev/null 2>&1
+  EXIT_CODE=$?
+  sleep 4
+done
+echo "cert-manager prod issuer available, continuing..."
+
 kubectl apply -f manifests/prod/prod-service.yaml
