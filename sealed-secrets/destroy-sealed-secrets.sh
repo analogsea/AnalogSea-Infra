dfe402870c3d17a648091fe580e9ef7340f3c90e
Clement Liaw <cklliaw@uwaterloo.ca>
2020-03-25 22:02:29 -0400

    Rename sealed-secrets Helm name to conform to CLI tool requirement

--- sealed-secrets/destroy-sealed-secrets.sh
+++ sealed-secrets/destroy-sealed-secrets.sh
@@ -1,3 +1,4 @@
 cd "${0%/*}"
 
-kubectl delete -f manifests/kubeseal.yaml
+#kubectl delete -f manifests/kubeseal.yaml
+helm uninstall sealed-secrets-controller -n kube-system
--- sealed-secrets/install-sealed-secrets.sh
+++ sealed-secrets/install-sealed-secrets.sh
@@ -1,4 +1,5 @@
 cd "${0%/*}"
 
-kubectl apply -f manifests/sealed-secrets-namespace.yaml
-kubectl apply -f manifests/kubeseal.yaml
+#kubectl apply -f manifests/sealed-secrets-namespace.yaml
+# kubectl apply -f manifests/kubeseal.yaml
+helm install sealed-secrets-controller stable/sealed-secrets -n kube-system -f manifests/sealed-secrets-vars.yaml
